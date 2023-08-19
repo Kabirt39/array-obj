e803e604263a620b101bce7431173ecfd39dfead
kabirt39 <mdshahjahan692@gmail.com>
2023-08-19 23:34:39 +0600

    intial

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const button = document.getElementById("button");
+
+const paragrap = document.querySelector(".paragrap");
+
+
+button.addEventListener("click", function(){
+    paragrap.innerHTML = "It's working";
+});
